699992e4e8d0ecdb2438249cdfaf7696fe6bd78e
Daramir95 <daramir1995@gmail.com>
2020-07-02 23:38:23 +0300

    Add Method DrawLine and DeletAll

--- Painter/WinxPainter.Designer.cs
+++ Painter/WinxPainter.Designer.cs
@@ -48,9 +48,10 @@
             this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox.Location = new System.Drawing.Point(0, 0);
             this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(1576, 538);
+            this.pictureBox.Size = new System.Drawing.Size(899, 538);
             this.pictureBox.TabIndex = 0;
             this.pictureBox.TabStop = false;
+            this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox_Paint);
             this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
             this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
             this.pictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
@@ -91,7 +92,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(42, 9);
+            this.label2.Location = new System.Drawing.Point(65, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(14, 13);
             this.label2.TabIndex = 4;
@@ -127,7 +128,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1576, 538);
+            this.ClientSize = new System.Drawing.Size(899, 538);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.Rubber);
             this.Controls.Add(this.DeletAll);
--- Painter/WinxPainter.cs
+++ Painter/WinxPainter.cs
@@ -12,26 +12,24 @@ namespace Painter
 {
     public partial class Painter : Form
     {
+        
         Color CurrentColor;
         Point CurrentPoint;
         Point PreviuosPoint;
         bool mouseDown;
-        
+        Point FirstPoint;
+        Point SecondPoint;
+
+         
 
         public Painter()
         {
             InitializeComponent();
+
             CurrentColor = Color.Red;
             StaticBitmap.Bitmap = new Bitmap(pictureBox.Width, pictureBox.Height);
-        }
-        private void PaintPixel(Point point)
-        {
-
-            StaticBitmap.Bitmap.SetPixel(point.X, point.Y, CurrentColor);
-            pictureBox.Image = StaticBitmap.Bitmap;
-        }
-
-
+           
+        }      
 
         private void Draw(Point PreviousPoint, Point CurrentPoint, Color color )
         {
@@ -75,9 +73,14 @@ namespace Painter
             //}
         }
 
+        private void DrawLine(Point PreviousPoint, Point CurrentPoint, Color color)
+        {
+            Draw(PreviousPoint, CurrentPoint, color);
+        }
         private void pictureBox_MouseDown(object sender, MouseEventArgs e)
         {
             mouseDown = true;
+
             if (toolBox.SelectedIndex == -1)
             {
                 MessageBox.Show("Вы не выбрали инструмент для рисования");
@@ -87,16 +90,13 @@ namespace Painter
                 CurrentPoint = e.Location;
             }
             else if (toolBox.SelectedIndex == 1)
-            {
-
-            }
-            
+            {                
+                FirstPoint = e.Location;
+            }            
         }
 
         private void pictureBox_MouseMove(object sender, MouseEventArgs e)
         {
-
-
             if (mouseDown)
             {
                 if (toolBox.SelectedIndex == 0)
@@ -107,11 +107,13 @@ namespace Painter
                 }
                 else if (toolBox.SelectedIndex == 1)
                 {
-
+                    
                 }
             }
+           
             label1.Text = $"X = {e.X}";
             label2.Text = $"Y = {e.Y}";
+           
         }
         private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
         {
@@ -120,13 +122,18 @@ namespace Painter
             {
                 
             }
-            else if (toolBox.SelectedIndex == 1) { }
+            else if (toolBox.SelectedIndex == 1) 
+            {               
+                SecondPoint = e.Location;               
+                DrawLine(FirstPoint, SecondPoint, CurrentColor);
+            }
 
         }
 
         private void Painter_Load(object sender, EventArgs e)
         {
-
+            
+            
         }
 
         private void ColorBox_Click(object sender, EventArgs e)
@@ -140,7 +147,13 @@ namespace Painter
 
         private void DeletAll_Click(object sender, EventArgs e)
         {
-            pictureBox.Refresh();
+            pictureBox.Image = null;
+            StaticBitmap.Bitmap = null;
+            StaticBitmap.Bitmap = new Bitmap(pictureBox.Width, pictureBox.Height);
+        }
+
+        private void pictureBox_Paint(object sender, PaintEventArgs e)
+        {
         }
     }
 }
