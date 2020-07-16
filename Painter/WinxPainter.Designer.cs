d08068910b7a168dbd78de7f201cf0b724b70eb7
Di <diana.iancheva@gmail.com>
2020-07-16 14:30:18 +0300

    Added side amount check for polygon entry

--- Painter/WinxPainter.Designer.cs
+++ Painter/WinxPainter.Designer.cs
@@ -190,6 +190,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(110, 20);
             this.textBox1.TabIndex = 10;
+            this.textBox1.Text = "Количество граней";
             // 
             // showAll
             // 
--- Painter/WinxPainter.cs
+++ Painter/WinxPainter.cs
@@ -12,7 +12,7 @@ namespace Painter
     public partial class Painter : Form
     {
         AFigure CurrentFigure;
-     
+
         StaticBitmap bitmap;
         IFigureFactory factoryFigure;
         Color _fillColor;
@@ -40,8 +40,9 @@ namespace Painter
             _fillColor = Color.White;
             bitmap.Bitmap = new Bitmap(pictureBox.Width, pictureBox.Height);
             bitmap.tmpBitmap = new Bitmap(pictureBox.Width, pictureBox.Height);
-           
-            textBox1.Text = "0";
+
+
+            textBox1.Hide();
         }
 
         private void DrawTree(double x, double y, double a, double angle)
@@ -67,7 +68,6 @@ namespace Painter
             }
         }
 
-
         private void pictureBox_MouseDown(object sender, MouseEventArgs e)
         {
             mouseDown = true;
@@ -75,12 +75,28 @@ namespace Painter
             if (toolBox.SelectedIndex != 6)
             {
                 CurrentFigure = null;
-                n = Convert.ToInt32(textBox1.Text);
+
+                if (toolBox.SelectedIndex == 4)
+                {
+                    
+                    try
+                    {
+                        n = Convert.ToInt32(textBox1.Text);
+                        if (n < 5)
+                        {
+                            mouseDown = false;
+                            MessageBox.Show("Минимальное количество граней = 5.");
+                        }
+                    }
+                    catch (FormatException)
+                    {
+                        MessageBox.Show("Введите число 5 или больше.");
+                        mouseDown = false;
+                    }
+                }
                 FirstPoint = e.Location;
             }
-
         }
-
         private void pictureBox_MouseClick(object sender, MouseEventArgs e)
         {
 
@@ -150,7 +166,7 @@ namespace Painter
                     {
                         bitmap.ShowOnTheScreen();
                         CurrentFigure.Move(delta);
-                        bitmap.DrawFigure(CurrentFigure);                        
+                        bitmap.DrawFigure(CurrentFigure);
                     }
                 }
                 else if (toolBox.SelectedIndex == 0)
@@ -161,7 +177,7 @@ namespace Painter
                     bitmap.DrawLine(FirstPoint, SecondPoint, _currentThickness, _currentColor);
                     bitmap.CopyInOld();
                     pictureBox.Image = bitmap.Bitmap;
-                    
+
                 }
                 else if (toolBox.SelectedIndex != 6)
                 {
@@ -175,7 +191,7 @@ namespace Painter
                     {
 
                         CurrentFigure.Update(e.Location);
-                        bitmap.DrawFigure(CurrentFigure);                      
+                        bitmap.DrawFigure(CurrentFigure);
 
                     }
                 }
@@ -259,33 +275,48 @@ namespace Painter
             switch (toolBox.SelectedIndex)
             {
                 case 1:
+                    textBox1.Hide();
                     factoryFigure = new LineFactory();
                     break;
                 case 2:
+                    textBox1.Hide();
                     factoryFigure = new RectangleFactory();
                     break;
                 case 3:
+                    textBox1.Hide();
                     factoryFigure = new SquareFactory();
                     break;
                 case 4:
+                    textBox1.Show();
+                    if (textBox1.Text == "Количество граней")
+                    {
+                        MessageBox.Show("Введите количество граней 5 или больше.");
+                    }
+                    textBox1.Text = "";
                     factoryFigure = new NSidedPolygonFactory();
                     break;
                 case 5:
+                    textBox1.Hide();
                     factoryFigure = new TrapezoidFactory();
                     break;
                 case 6:
+                    textBox1.Hide();
                     factoryFigure = null;
                     break;
                 case 7:
+                    textBox1.Hide();
                     factoryFigure = new RightTriangleFactory();
                     break;
                 case 8:
+                    textBox1.Hide();
                     factoryFigure = new IsoscelesTriangleFactory();
                     break;
                 case 10:
+                    textBox1.Hide();
                     factoryFigure = new CircleFactory();
                     break;
                 case 11:
+                    textBox1.Hide();
                     factoryFigure = new EllipseFactory();
                     break;
             }
